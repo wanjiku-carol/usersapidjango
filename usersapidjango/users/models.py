7241bb95398185c5a58503f4f67260eb7e3a7cbc
Caroline Wanjiku <carolchege4300@gmail.com>
2022-10-05 17:14:36 +0300

    Modify(Extend) the user model and create superuser account

--- usersapidjango/users/models.py
+++ usersapidjango/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, DateField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -15,6 +15,8 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    location = CharField(max_length=30, blank=True)
+    birth_date = DateField(blank=True, null=True)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
